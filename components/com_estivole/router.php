f1d5271eefd785966c2a025f352d2a667bca3cec
gorgozilla <ljeanmonod@gmail.com>
2014-10-28 22:59:41 +0100

    Add Joomla3

---
+++ components/com_estivole/router.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * @package     Joomla.Site
+ * @subpackage  com_contact
+ *
+ * @copyright   Copyright (C) 2005 - 2014 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+defined('_JEXEC') or die;
+
+/**
+ * Routing class from com_contact
+ *
+ * @package     Joomla.Site
+ * @subpackage  com_contact
+ * @since       3.3
+ */
+class EstivoleRouter extends JComponentRouterBase
+{
+
+}
